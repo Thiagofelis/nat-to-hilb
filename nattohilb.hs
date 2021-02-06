401693fce02340c9423bc300eb7778ddd6f0aba5
Thiago Felicissimo <thiagofelis-br@hotmail.com>
2021-02-06 19:55:42 +0100

    type inference in hilbert system working

--- nattohilb.hs
+++ nattohilb.hs
@@ -23,40 +23,12 @@ data Type = Base Int
           | Sum Type Type
           | Bot deriving (Eq, Read, Show)
 
-data HollowType = Hole Int
-                | ArrowH HollowType HollowType
-                | ProductH HollowType HollowType
-                | SumH HollowType HollowType
-                | BotH deriving (Eq, Read, Show)
-                
-
-data HollowHilb = AxH Int HollowType
-          | MPH HollowHilb HollowHilb HollowType
-          | SH HollowType | KH HollowType
-          | IandH HollowType | ERandH HollowType | ELandH HollowType
-          | IRorH HollowType | ILorH HollowType  | EorH HollowType
-          | ExFH HollowType deriving (Eq, Read, Show)
-
 data TypedHilb = AxT Int Type
                | MPT TypedHilb TypedHilb Type
                | ST Type | KT Type
                | IandT Type | ERandT Type | ELandT Type
                | IRorT Type | ILorT Type  | EorT Type
                | ExFT Type deriving (Eq, Read, Show)
-
-
---data Ctx = Empty
---         | Cons Type Int
-
-type Ctx = Hilb -> Maybe Type
-
---data UniEq = Null
---           | Cons Type Type UniEq
-
-data UnifyList = Nil
-               | Cons HollowType HollowType UnifyList
-
-          
 
 allvar :: NatDed -> Int -> Bool
 allvar (Var y) x = if (x == y) then True else False
@@ -139,96 +111,167 @@ red (MP x y) = let reda = red x in
                      MP a b -> MP a b
 red c = c
 
-appearsin :: HollowType -> Int -> Bool
-appearsin (Hole n) m = (n == m)
-appearsin (ArrowH x y) m = or [appearsin x m, appearsin y m]
-appearsin (ProductH x y) m = or [appearsin x m, appearsin y m]
-appearsin (SumH x y) m = or [appearsin x m, appearsin y m]
-appearsin BotH m = False
+appearsin :: Type -> Int -> Bool
+appearsin (Base n) m = (n == m)
+appearsin (Arrow x y) m = or [appearsin x m, appearsin y m]
+appearsin (Product x y) m = or [appearsin x m, appearsin y m]
+appearsin (Sum x y) m = or [appearsin x m, appearsin y m]
+appearsin Bot m = False
 
-subs :: HollowType -> Int -> HollowType -> HollowType
-subs (Hole n) m y = If (n == m) then y else (Hole n)
-subs (ArrowH x1 x2) m y = ArrowH (subs x1 m y) (subs x2 m y)
-subs (ProductH x1 x2) m y = ProductH (subs x1 m y) (subs x2 m y)
-subs (SumH x1 x2) m y = SumH (subs x1 m y) (subs x2 m y)
-subs BotH m y = BotH
+subs :: Type -> Int -> Type -> Type
+subs (Base n) m y = if (n == m) then y else (Base n)
+subs (Arrow x1 x2) m y = Arrow (subs x1 m y) (subs x2 m y)
+subs (Product x1 x2) m y = Product (subs x1 m y) (subs x2 m y)
+subs (Sum x1 x2) m y = Sum (subs x1 m y) (subs x2 m y)
+subs Bot m y = Bot
 
-unify :: [(HollowType, HollowType)] -> [(HollowType, HollowType)] -> Maybe [(HollowType, HollowType)]
-unify l1 ((x, Hole n) : l2) = unify l1 ((Hole n, x) : l2)
-unify l1 (((ArrowH x1 x2), y) : l2) = case y of
-                                        ArrowH y1 y2 -> unify l1 ((x1, y1) : (x2, y2) : l2)
-                                        _ -> Nothing
-unify l1 (((ProductH x1 x2), y) : l2) = case y of
-                                          ProductH y1 y2 -> unify l1 ((x1, y1) : (x2, y2) : l2)
-                                          _ -> Nothing
-unify l1 (((SumH x1 x2), y) : l2) = case y of
-                                      SumH y1 y2 -> unify l1 ((x1, y1) : (x2, y2) : l2)
-                                      _ -> Nothing
-unify l1 ((BotH, y) : l2) = case x of
-                              BotH -> unify l1 l2
-                              _ -> Nothing
-unify l1 ((Hole n, x) : l2) = if (appearsin x n) then
+
+unify :: [(Type, Type)] -> [(Type, Type)] -> Maybe [(Type, Type)]
+unify l1 ((Base n, x) : l2) = if (appearsin x n) then
                                 Nothing
                               else
                                 let mapsubs = (map (\w -> let (w1, w2) = w in (subs w1 n x, subs w2 n x))) in
                                   let appearsinlist = (\lx -> or $ map (\w -> let (w1, w2) = w in or [appearsin w1 n, appearsin w2 n]) lx) in
                                     if (not $ appearsinlist l1) then
-                                      unify ((Hole n, x) : l1) (mapsubs l2)
+                                      unify ((Base n, x) : l1) (mapsubs l2)
                                     else
-                                      unify [] ((mapsubs l1) ++ (Hole n, x) ++ (mapsubs l2))
+                                      unify [] ((mapsubs l1) ++ [(Base n, x)] ++ (mapsubs l2))
+unify l1 ((x, Base n) : l2) = unify l1 ((Base n, x) : l2)
+unify l1 (((Arrow x1 x2), y) : l2) = case y of
+                                       Arrow y1 y2 -> unify l1 ((x1, y1) : (x2, y2) : l2)
+                                       _ -> Nothing
+unify l1 (((Product x1 x2), y) : l2) = case y of
+                                         Product y1 y2 -> unify l1 ((x1, y1) : (x2, y2) : l2)
+                                         _ -> Nothing
+unify l1 (((Sum x1 x2), y) : l2) = case y of
+                                     Sum y1 y2 -> unify l1 ((x1, y1) : (x2, y2) : l2)
+                                     _ -> Nothing
+unify l1 ((Bot, y) : l2) = case y of
+                             Bot -> unify l1 l2
+                             _ -> Nothing
 unify l1 [] = Just l1 -- end condition
 
 
-typeinf :: Hilb -> Int -> Maybe (HollowHilb, Int)
-typeinf (Ax x) f n = Nothing -- we reject open terms
-typeinf K f n = Just (KH (ArrowH
-                         (Hole n)
-                         (ArrowH
-                           (Hole (n + 1))
-                           (Hole n))),                         
+replace :: TypedHilb -> Int -> Type -> TypedHilb
+replace (AxT m x) n y = AxT m (subs x n y)
+replace (ST x) n y = ST (subs x n y)
+replace (KT x) n y = KT (subs x n y)
+replace (IandT x) n y = IandT (subs x n y)
+replace (ERandT x) n y = ERandT (subs x n y)
+replace (ELandT x) n y = ELandT (subs x n y)
+replace (IRorT x) n y = IRorT (subs x n y)
+replace (ILorT x) n y = ILorT (subs x n y)
+replace (EorT x) n y = EorT (subs x n y)
+replace (ExFT x) n y = ExFT (subs x n y)
+replace (MPT a b x) n y = MPT (replace a n y) (replace b n y) (subs x n y)
+
+gettype :: TypedHilb -> Type
+gettype (AxT m x) = x
+gettype (ST x) = x
+gettype (KT x) = x
+gettype (IandT x) = x
+gettype (ERandT x) = x
+gettype (ELandT x) = x
+gettype (IRorT x) = x
+gettype (ILorT x) = x
+gettype (EorT x) = x
+gettype (ExFT x) = x
+gettype (MPT a b x) = x
+
+
+matchholes :: TypedHilb -> TypedHilb -> Maybe TypedHilb
+matchholes a b = let ta = gettype a in
+                     case ta of
+                       Base n -> let tb = gettype b in         
+                                   let nt = Arrow tb (Base n) in
+                                     let na = replace a n nt in
+                                       Just (MPT na b (Base n))
+                       Arrow t1 t2 -> let tb = gettype b in  
+                                        let ntb = unify [] [(t1, tb)] in
+                                          ntb >>= (\x ->
+                                                     let na = (foldl
+                                                               (\y w -> let (w1, w2) = w in case w1 of Base k -> replace y k w2)
+                                                               a x) in
+                                                       let nb = (foldl
+                                                                 (\y w -> let (w1, w2) = w in case w1 of Base k -> replace y k w2)
+                                                                 b x) in
+                                                         let nt = case (gettype na) of Arrow t3 t4 -> t4 in
+                                                           Just (MPT na nb nt))
+                       _ -> Nothing
+                                     
+
+typeinf :: Hilb -> Int -> Maybe (TypedHilb, Int)
+typeinf (Ax x) n = Nothing -- we reject open terms
+typeinf K n = Just (KT (Arrow
+                        (Base n)
+                        (Arrow
+                          (Base (n + 1))
+                          (Base n))),                         
                      n + 2)
---typeinf S f = (SH (ArrowH (
-typeinf Iand f n = Just (IandH (ArrowH
-                               (Hole n)
-                               (ArrowH
-                                (Hole (n + 1))
-                                (ProductH
-                                 (Hole n)
-                                 (Hole (n + 1))))),
+typeinf S n = Just (ST (Arrow
+                         (Arrow
+                           (Base n)
+                           (Arrow
+                             (Base (n + 1))
+                             (Base (n + 2))))
+                         (Arrow
+                           (Arrow
+                             (Base n)
+                             (Base (n + 1)))
+                           (Arrow
+                             (Base n)
+                             (Base (n + 2))))),
+                     n + 3)
+typeinf Iand n = Just (IandT (Arrow
+                               (Base n)
+                               (Arrow
+                                 (Base (n + 1))
+                                 (Product
+                                   (Base n)
+                                   (Base (n + 1))))),
+                        n + 2)                                 
+typeinf ERand n = Just (ERandT (Arrow
+                                 (Product (Base n) (Base (n + 1)))
+                                 (Base (n + 1))),
+                         n + 2)
+typeinf ELand n = Just (ELandT (Arrow
+                                 (Product (Base n) (Base (n + 1)))
+                                 (Base n)),
+                         n + 2)
+typeinf IRor n = Just (IRorT (Arrow
+                               (Base (n + 1))
+                               (Sum (Base n) (Base (n + 1)))),
                         n + 2)
-typeinf (MP a b) f n = do
-  resa <- typeinf a f n
-  let (ta,na) = resa
-  resb <- typeinf b f na
+typeinf ILor n = Just (ILorT (Arrow
+                               (Base n)
+                               (Sum (Base n) (Base (n + 1)))),
+                        n + 2)
+typeinf Eor n = Just (EorT (Arrow
+                             (Sum (Base n) (Base (n + 1)))
+                             (Arrow
+                               (Arrow (Base n) (Base (n + 2)))
+                               (Arrow
+                                 (Arrow (Base (n + 1)) (Base (n + 2)))
+                                 (Base (n + 2))))),
+                       n + 3)
+typeinf ExF n = Just (ExFT (Arrow (Bot) (Base n)), n + 1)                             
+typeinf (MP a b) n = do
+  resa <- typeinf a n
+  let (ta, na) = resa
+  resb <- typeinf b na
   let (tb, nb) = resb
-  return (matchholes ta tb, nb)
-  
-
---typeinf (MP a b) f n = let (ta, m) = typeinf a f n in
---                        let (tb, o) = typeinf b f m in
---  a                        case ta of
---                            _ 
-
-
---putbases :: Hilb -> Int -> (HilbAnnotated, Int)
---putbases (Ax x) n = (AxT x (Base n), n + 1)
---putbases (MP a b) n = let (ta, m) = putbases a n in
---                        let (tb, o) = putbases b m in
---                          (MPT ta tb (Base o), o + 1)
---putbases c n = case c of
---                 K -> (KT (Base n) (Base (n + 1)), n + 2)
---                 S -> (ST (Base n) (Base (n + 1)) (Base (n + 2)), n + 3)
---                 Iand -> (IandT (Base n) (Base (n + 1)), n + 2)
---                 ELand -> (ELandT (Base n) (Base (n + 1)), n + 2)
---                 ERand -> (ERandT (Base n) (Base (n + 1)), n + 2)
---                 Eor -> (EorT (Base n) (Base (n + 1)) (Base (n + 2)), n + 3)
---                 ILor -> (ILorT (Base n) (Base (n + 1)), n + 1)
---                 IRor -> (IRorT (Base n) (Base (n + 1)), n + 1)
---                 ExF -> (ExFT (Base n), n + 1)
+  nt <- matchholes ta tb
+  return (nt, nb)
 
 
 -- proof of (not A \/ B) -> A -> B
-test = Abs 0 (Abs 1 (Case (Var 0) (Exfalso (App (Var 0) (Var 1))) (Var 0)))
+test = Abs 0 (Abs 1 (Case
+                      (Var 0)
+                      (Abs 3 (Exfalso
+                               (App (Var 3) (Var 1)) ))
+                      (Abs 4 (Var 4))))
+-- proof of A -> ( (not A) -> B )
+test1 = Abs 0 (Abs 1 (Exfalso (App (Var 1) (Var 0))))
 -- A -> B -> A
 test2 = Abs 1 (Abs 2 (Var 1))
 -- A -> B -> C -> C
@@ -239,7 +282,11 @@ test4 = Abs 2 (Abs 0 (Var 0))
 abba = Abs 0 (Prod (Pi2 (Var 0)) (Pi1 (Var 0)))
 -- (False /\ B) -> C
 test5 = Abs 0 (Exfalso (Pi1 (Var 0)))
+-- (A /\ B) -> (A \/ B)
+test6 = Abs 0 (Inl (Pi1 (Var 0)))
+-- A -> A
+test7 = Abs 3 (Var 3)
 
-
+err = Abs 0 (App (Var 0) (Var 0))
 
        
